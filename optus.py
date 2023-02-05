43336d644a548d682b279aa7eaa2e2ff78b5d5c5
“kamesh20” <kamesh20k@gmail.com>
2023-02-05 03:55:15 +0000

    3rdcommit

--- optus.py
+++ optus.py
@@ -10,7 +10,7 @@ rev_str = reversed(my_str)
 
 # check if the string is equal to its reverse
 if list(my_str) == list(rev_str):
-   print("The string is a palindrome.")
+   print("The stringg is a palindrome.")
 else:
    print("The string is not a palindrome.")
 
